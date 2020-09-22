7d55d6822818df2cff5f2272840a80810e82f726
Miu <miu@MacBook-Pro.hitronhub.home>
2020-09-21 21:25:58 -0700

    first commit

--- function6.js
+++ function6.js
@@ -1,11 +1,10 @@
-// write a function that takes the last item of the second array, 
-// and adds it to the end of the first array.
+// write a function that takes the last item of the second array, and adds it to the end of the first array.
 let food = ['apple', 'orange', 'cake', 'carrots', 'pasta', 'rice', 'eggs'];
 
 let numbers = ['1', '2', '3', '4'];      
 
 const addTheLast = function (arr1, arr2) {
-    arr1.push(arr2[3]);
+    arr1.push(arr2[arr2.length -1])
     console.log(arr1);
 };
 
